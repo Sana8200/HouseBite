5d0c7ef07fd84c01a79ee5cd8e0e5b107ec7fd4a
David Turesson <davidtur@kth.se>
2026-04-13 13:08:27 +0200

    basic header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import './App.css'
 import { Test } from './pages/test/Test'
 import { Landing } from "./pages/landing/Landing";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route index element={<Landing />} />
         <Route path="test" element={<Test />} />
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,20 @@
+
+import { NavLink } from "react-router"
+import "./Header.css"
+
+export function Header() {
+    return (
+        <div className="header">
+            <NavLink to="/">
+                HouseBite
+            </NavLink>
+            <div className="header-spacer"/>
+            { import.meta.env.DEV &&
+                <NavLink to="/test">Test</NavLink>
+            }
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Sign up</NavLink>
+        </div>
+    )
+}
