7e6292b0e3f6e1b61d61c825b60f662cf60d58a9
David Turesson <davidtur@kth.se>
2026-04-13 12:40:18 +0200

    move out test code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,65 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import { supabase } from './supabase'
+import { Test } from './Test'
 
-function App() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [text, setText] = useState("test")
-
-  const loginClick = async () => {
-    const result = await supabase.auth.signInWithPassword({
-      email,
-      password
-    })
-
-    if (result.error) {
-      window.alert(result.error)
-      return;
-    }
-
-    window.alert("OK")
-  }
-
-  const helloClick = async () => {
-    const result = await supabase.functions.invoke<{message: string}>("hello-world", {
-      body: {
-        name: text,
-      }
-    })
-
-    if (result.error) {
-      window.alert(result.error)
-      return;
-    }
-
-    window.alert(result.data!.message);
-  }
-
+export function App() {
   return (
     <>
-      <div>
-        <p>Login</p>
-
-        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-
-        <button onClick={() => void(loginClick())}>
-          Login
-        </button>
-      </div>
-
-      <div>
-        <p>Hello</p>
-
-        <input value={text} onChange={(e) => setText(e.target.value)}/>
-
-        <button onClick={() => void(helloClick())}>
-          Test
-        </button>
-      </div>
+      <Test/>
     </>
   )
 }
-
-export default App
