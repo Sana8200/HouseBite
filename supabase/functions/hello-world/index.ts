1c1fefa1794ada4c0e4c2b42c451cde83b2caba8
David Turesson <davidtur@kth.se>
2026-04-13 11:43:22 +0200

    test supabase

--- supabase/functions/hello-world/index.ts
+++ supabase/functions/hello-world/index.ts
@@ -5,9 +5,15 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 import "@supabase/functions-js/edge-runtime.d.ts"
 
-console.log("Hello from Functions!")
-
 Deno.serve(async (req) => {
+  if (req.method == "OPTIONS") {
+    return new Response(null, {headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+    }})
+  }
+
   const { name } = await req.json()
   const data = {
     message: `Hello ${name}!`,
@@ -18,15 +24,3 @@ Deno.serve(async (req) => {
     { headers: { "Content-Type": "application/json" } },
   )
 })
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/hello-world' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
