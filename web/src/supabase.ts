1f85bd44b98cc8cc6f2f2e0e233aa9efcda534d5
David Turesson <davidtur@kth.se>
2026-04-14 13:40:56 +0200

    Alert on missing .env file

--- web/src/supabase.ts
+++ web/src/supabase.ts
@@ -3,6 +3,10 @@ import { createClient, type User } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string;
 
+if (!supabaseUrl || !supabaseKey) {
+    window.alert("Missing .env file!");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
