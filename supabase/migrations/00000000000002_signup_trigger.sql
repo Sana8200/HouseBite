ad784025039b24f89c647f220e8b3103a84284cb
Sana Monhaseri <s.monhaserii@gmail.com>
2026-04-20 12:56:01 +0200

    Revert signup trigger to original

--- supabase/migrations/00000000000002_signup_trigger.sql
+++ supabase/migrations/00000000000002_signup_trigger.sql
@@ -2,16 +2,13 @@
 CREATE OR REPLACE FUNCTION handle_new_user()
 RETURNS trigger AS $$
 BEGIN
-  INSERT INTO public.family_member (id, display_name, email)
+  INSERT INTO public.family_member (id)
   VALUES (
-    NEW.id,
-    NEW.raw_user_meta_data->>'display_name',
-    NEW.email
+    NEW.id
   );
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
-
 -- 2. Attach it to auth.users
 CREATE OR REPLACE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
