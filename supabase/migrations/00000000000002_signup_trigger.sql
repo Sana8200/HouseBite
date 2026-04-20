97cb307492e8e80611ba57aaf6a35fc77bd4dbbc
Sana Monhaseri <s.monhaserii@gmail.com>
2026-04-20 12:49:46 +0200

    Update signup trigger to populate display_name and email

--- supabase/migrations/00000000000002_signup_trigger.sql
+++ supabase/migrations/00000000000002_signup_trigger.sql
@@ -2,13 +2,16 @@
 CREATE OR REPLACE FUNCTION handle_new_user()
 RETURNS trigger AS $$
 BEGIN
-  INSERT INTO public.family_member (id)
+  INSERT INTO public.family_member (id, display_name, email)
   VALUES (
-    NEW.id
+    NEW.id,
+    NEW.raw_user_meta_data->>'display_name',
+    NEW.email
   );
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
+
 -- 2. Attach it to auth.users
 CREATE OR REPLACE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
