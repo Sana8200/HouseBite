9e7180e95eb7751635d798beb09aed08f7a1d9c6
David Turesson <davidtur@kth.se>
2026-04-28 15:11:48 +0200

    add project link

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -6,6 +6,7 @@ export function Footer() {
             <Divider/>
             <Flex p="xl" justify="center" wrap="wrap" gap="xl">
                 <Text c="dimmed">© HouseBite, KTH II1305 Riebnes</Text>
+                <Text c="gray.7" component="a" href="https://arpega75.github.io/houseBite/" target="_blank">About</Text>
             </Flex>
         </Box>
     );
