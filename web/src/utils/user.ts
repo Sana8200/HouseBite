2266fdd66e70469389b45a13e8516b599dedbf49
David Turesson <davidtur@kth.se>
2026-05-04 15:27:02 +0200

    get and set avatars

--- web/src/utils/user.ts
+++ web/src/utils/user.ts
@@ -1,5 +1,11 @@
 import type { User } from "@supabase/supabase-js";
 
+const avatars = import.meta.glob("../assets/avatars/*.png", {
+    eager: true,
+    query: "?url",
+    import: "default",
+});
+
 export function getUsername(user: User): string {
     return (user.user_metadata?.display_name as string | undefined) ??
         (user.user_metadata?.username as string | undefined) ??
@@ -8,5 +14,9 @@ export function getUsername(user: User): string {
 }
 
 export function getAvatar(user: User): string | undefined {
-    return user.user_metadata?.avatar_url as string | undefined;
+    const id = user.user_metadata?.avatar_id as string | undefined;
+    if (!id) return undefined;
+
+    const avatar = avatars[`../assets/avatars/${id}.png`] as string | undefined;
+    return avatar;
 }
